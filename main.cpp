99c24f12d809ff4b8c710594a0e4e4b618b53668
Rafa Pagés <blinkmenow@gmail.com>
2014-03-31 11:45:39 +0200

    Two objects are now detected (one in each half of the screen)

--- main.cpp
+++ main.cpp
@@ -10,13 +10,16 @@ int main(int argc, char *argv[]){
     Mat frame = image;
 
     Ball ball(image->height, image->width, 20, 20);
-    Tracker tracker;
+    Tracker leftTracker, rightTracker;
 
     cvNamedWindow("MainWindow", CV_WINDOW_AUTOSIZE);
     cvShowImage("MainWindow", image);
 
-    cvNamedWindow("BIN", CV_WINDOW_AUTOSIZE);
-
+    // The image is divided into two halves to track the ball in each
+    IplImage* leftHalfImage  = cvCreateImage(cvSize(image->width/2, image->height), image->depth, image->nChannels);
+    IplImage* rightHalfImage = cvCreateImage(cvSize(image->width/2, image->height), image->depth, image->nChannels);
+    CvMat tmp;
+    Mat leftHalfFrame, rightHalfFrame;
 
     while(true){
 
@@ -24,21 +27,31 @@ int main(int argc, char *argv[]){
         image = capture.nextVideoFrame();
         frame = image;
 
-        //
-        tracker.setFrames(frame);
-        tracker.binFrameProcessing();
-        Mat bin = tracker.getBinFrame();
-        Point posT = tracker.trackObject();
-        imshow("BIN", bin);
-        //
+        // Half frames are set
+        cvGetSubRect(image, &tmp, cvRect(0,0, leftHalfImage->width, leftHalfImage->height));
+        cvCopy(&tmp, leftHalfImage);
+        leftHalfFrame = leftHalfImage;
+
+        cvGetSubRect(image, &tmp, cvRect(leftHalfImage->width, 0, rightHalfImage->width, rightHalfImage->height));
+        cvCopy(&tmp, rightHalfImage);
+        rightHalfFrame = rightHalfImage;
+
+        // Object is tracked in both half frames
+        leftTracker.setFrames(leftHalfFrame);
+        leftTracker.binFrameProcessing();
+        rightTracker.setFrames(rightHalfFrame);
+        rightTracker.binFrameProcessing();
+        Point posL = leftTracker.trackObject();
+        Point posR = rightTracker.trackObject();
 
         if (image == 0)
             break;
 
         Point pos = ball.getPosition();
 
-        // is the tennis ball tracked?
-        circle(frame, posT, 5, Scalar(0,255,0), -1);
+        // A small circle is drawn in the center of the balls
+        circle(frame, posL, 5, Scalar(0,255,0), -1);
+        circle(frame, posR + Point(leftHalfImage->width, 0), 5, Scalar(0,255,0), -1);
 
         // We draw a ball in the screen
         circle(frame, Point(pos.y, pos.x), ball.getRadious(), Scalar(0,0,255), -1);
